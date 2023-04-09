b540d6f576487493515f75eb60ab72876e3e0f14
Wenveo <Wenveo@outlook.com>
2023-04-09 22:07:03 +0800

    Add zero runtime native class library sample.

---
+++ CustomDllMain/CustomDllMain.cpp
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#include <stdio.h>
+#include <stdint.h>
+#include <Windows.h>
+
+extern "C" void OnLoaded();
+extern "C" void OnUnloaded();
+
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved )  // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch( fdwReason )
+    {
+        case DLL_PROCESS_ATTACH:
+         // Initialize once for each new process.
+         // Return FALSE to fail DLL load.
+            OnLoaded();
+            break;
+
+        case DLL_THREAD_ATTACH:
+         // Do thread-specific initialization.
+            break;
+
+        case DLL_THREAD_DETACH:
+         // Do thread-specific cleanup.
+            break;
+
+        case DLL_PROCESS_DETACH:
+         // Perform any necessary cleanup.
+            OnUnloaded();
+            break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
