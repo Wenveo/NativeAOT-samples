886f3a2e76b8debb0570cec37592d69cef771fef
Wenveo <Wenveo@outlook.com>
2023-04-10 10:27:14 +0800

    Add LoadLibraryTest proj

---
+++ Tests/LoadLibraryTest/LoadLibraryTest.c
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    HINSTANCE handle = LoadLibraryA("..\\..\\NativeLibrary-ZeroRuntime\\bin\\Release\\net8.0\\win-x64\\publish\\NativeLibrary.dll");
+
+    typedef int(*funcAdd)(int,int);
+    funcAdd add = (funcAdd)GetProcAddress(handle, "Add");
+
+    int result = add(11, 22);
+    printf("result: %i\n", result);
+
+    FreeLibrary(handle);
+    return 0;
+}
