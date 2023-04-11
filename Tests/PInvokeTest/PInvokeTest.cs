c8361d83dfbfc282755c5ff7709f829bc447a11a
Wenveo <Wenveo@outlook.com>
2023-04-11 23:26:07 +0800

    Lowercase all top-level folder names.

--- Tests/PInvokeTest/PInvokeTest.cs
+++ Tests/PInvokeTest/PInvokeTest.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 public class PInvokeTest
 {
-    [DllImport(@"..\..\..\..\..\Samples\NativeLibrary-ZeroRuntime\bin\Release\net8.0\win-x64\publish\NativeLibraryZ.dll")]
+    [DllImport(@"..\..\..\..\..\samples\NativeLibrary-ZeroRuntime\bin\Release\net8.0\win-x64\publish\NativeLibraryZ.dll")]
     private static extern int Add(int a, int b);
 
     [Fact]
